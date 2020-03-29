d39ef461a497e0dac599c0a60acc0c5f793a9af8
Shubham Singhal <shubham.011190@gmail.com>
2020-03-29 15:57:34 +0530

    items access from DB

--- app.js
+++ app.js
@@ -29,10 +29,26 @@ const defBlog = new Blog({
   title:"Hello",
   body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 })
-defBlog.save();
+
+const defItems = [defBlog];
 
 app.get("/", function(req,res){
-  res.render("home",{homecontent : homeStartingContent, postsVal: ""});
+  Blog.find({},function(err,fitems){
+    if(fitems.length==0){
+      Blog.insertOne(defBlog,function(err){
+        if(err){
+          console.log(err);
+        }
+        else {
+          console.log("Def items added successfully");
+        }
+      })
+      res.redirect("/");
+    }
+    else {
+        res.render("home",{homecontent : homeStartingContent, postsVal: fitems});
+    }
+  })
   // console.log(posts);
 })
 
